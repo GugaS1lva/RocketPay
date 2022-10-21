097aa270fc4f8d74bf6c07c8527341a0ec8bf497
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-10-20 22:13:01 -0300

    filtering the inputs [IMask]

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -9,8 +10,8 @@ function setCardType(type) {
     const colors = {
         visa: ['#17283A', '#D9BB7D'],
         mastercard: ['#E3001A', '#EF991B'],
-        nubank: ['#7E0ACA', '#8605B8'],
-        neon: ['#11A9F3', '#12DCDF'],
+        // nubank: ['#7E0ACA', '#8605B8'],
+        // neon: ['#11A9F3', '#12DCDF'],
         default: ['black', 'gray'],
     }
 
@@ -20,6 +21,71 @@ function setCardType(type) {
     ccLogo.setAttribute('src', `cc-${type}.svg`)
 }
 
-setCardType()
+globalThis.setCardType = setCardType
 
-globalThis.setCardType() = setCardType
+const securityCode = document.getElementById('security-code')
+const securityCodePattern = {
+    mask: "0000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.getElementById('expiration-date')
+const expirationDatePattern = {
+    mask: "MM{/}YY",
+    blocks: {
+        MM: {
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 12,
+        },
+        YY: {
+            mask: IMask.MaskedRange,
+            from: String(new Date().getFullYear()).slice(2),
+            to: String(new Date().getFullYear() + 10).slice(2),
+        }
+    }
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.getElementById('card-number')
+const cardNumberPattern = {
+    mask: [
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /^4\d{0,15}/,
+            cardtype: "visa",
+        },
+
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,2}/,
+            cardtype: "mastercard",
+        },
+
+        // {
+        //     mask: "0000 0000 0000 0000",
+        //     regex: /^4\d{0,15}/,
+        //     cardtype: "nubank",
+        // },
+
+        // {
+        //     mask: "0000 0000 0000 0000",
+        //     regex: /^4\d{0,15}/,
+        //     cardtype: "neon",
+        // },
+
+        {
+            mask: "0000 0000 0000 0000",
+            cardtype: "default"
+        },
+    ],
+    dispatch: function(appended, dynamicMasked){
+        const number = (dynamicMasked.value + appended).replace(/\D/g,'')
+        const foundMask = dynamicMasked.compiledMasks.find(function(item){
+            return number.match(item.regex)
+        })
+
+        return foundMask
+    }
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
